f94f4721a3aadac10a2118bc6d1302f1291e3301
ThePrincipalCodez <wisdomiwunwa2@gmail.com>
2024-10-04 20:56:37 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./app/**/*.{js,ts,jsx,tsx,mdx}",
-];
-export const theme = {
-  extend: {
-    colors: {
-      background: "var(--background)",
-      foreground: "var(--foreground)",
+module.exports = {
+  important: true,
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
   },
+  plugins: [],
 };
-export const plugins = [];
